e3a5131d7386038586685326f15304e3129f45c7
vikas0296 <91872517+vikas0296@users.noreply.github.com>
2022-01-04 20:44:02 +0100

    updated

---
+++ Stress_Strains.py
@@ -0,0 +1,51 @@
+import numpy as np
+from scipy import linalg
+import new_crack
+import sys
+import uniform_mesh
+
+def strain_stress_tensors(Uxy, Nodes, GaussPoint_1to4, D_plane_stress):
+
+
+    for node in Nodes:
+        print(node)
+
+        for points in GaussPoint_1to4:
+            xi_1 = points[0]
+            xi_2 = points[1]
+
+
+            dNdxi = (1/4) * np.array([[-(1-xi_2), 1-xi_2, 1+xi_2, -(1+xi_2)],
+                                      [-(1-xi_1), -(1+xi_1), 1+xi_1, 1-xi_1]])
+
+            jacobi = np.matmul(dNdxi, node)
+
+            inverse_jacobi = linalg.inv(jacobi)
+
+            dN = np.matmul(inverse_jacobi, dNdxi)
+
+            B_std = np.array([[dN[0, 0], 0, dN[0, 1], 0, dN[0, 2], 0, dN[0, 3], 0],
+                              [0, dN[1, 0], 0, dN[1, 1], 0, dN[1, 2], 0, dN[1, 3]],
+                              [dN[1, 0], dN[0, 0], dN[1, 1], dN[0, 1], dN[1, 2], dN[0, 2], dN[1, 3], dN[0, 3]]])
+
+            strains = np.matmul(B_std, Uxy[0])
+            print("-=-=-=-=-=-=-=-=")
+            stresses  = np.matmul(D_plane_stress, strains)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
